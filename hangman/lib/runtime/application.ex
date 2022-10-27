5a901ce7b0da6b82b06e5d29f5d192dc88ac09c3
NathnaelDS <nathnaelds@gmail.com>
2022-10-27 08:34:09 +0300

    make hangman a service using a dynamic supervisor

---
+++ hangman/lib/runtime/application.ex
@@ -0,0 +1,15 @@
+defmodule Hangman.Runtime.Application do
+  @supervisor_name GameStarter
+
+  use Application
+
+  def start(_type, _args) do
+    supervisor_spec = [{DynamicSupervisor, strategy: :one_for_one, name: @supervisor_name}]
+
+    Supervisor.start_link(supervisor_spec, strategy: :one_for_one)
+  end
+
+  def start_game do
+    DynamicSupervisor.start_child(@supervisor_name, {Hangman.Runtime.Server, nil})
+  end
+end
